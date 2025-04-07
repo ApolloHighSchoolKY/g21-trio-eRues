d71ed7d4c83ed25e249811b9641aa0615b3cbe27
eRues <ellie.rue@stu.daviess.kyschools.us>
2025-04-07 14:27:25 +0000

    finished

--- Trio.java
+++ Trio.java
@@ -1,1 +1,48 @@
 //Write your answer here
+public class Trio {
+    Sandwich sandwich;
+    Salad salad;
+    Drink drink;
+    private double price;
+    private String name;
+
+    public Trio(Sandwich mySandwich, Salad mySalad, Drink myDrink) {
+        sandwich = mySandwich;
+        salad = mySalad;
+        drink = myDrink;
+
+        if(sandwich < salad)
+        {
+            if(salad < drink)
+            {
+                price = salad.getPrice() + drink.getPrice();
+            }
+            else
+            {
+                price = salad.getPrice() + sandwich.getPrice();
+            }
+        }
+        else
+        {
+            if(salad > drink)
+            {
+                price = salad.getPrice() + sandwich.getPrice();
+            }
+            else
+            {
+                price = drink.getPrice() + sandwich.getPrice();
+            }
+        }
+
+        name = "" + sandwich.getname() + "/" + salad.getName() + "/" + drink.getName() + " Trio";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+}
